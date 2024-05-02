ce41ae3f9477c3cc7ee769ad64f5fdfcab37e549
RadionBr <radionbr@gmail.com>
2024-05-02 21:00:58 +0300

    Initial Commit

---
+++ main.py
@@ -0,0 +1,16 @@
+print('Hello World!')
+
+print("Hello\nworld")
+
+print("Hello\n\tworld")
+
+print("Hello\\n\\t\"world\"")
+
+print("\\\\\\\\\\")
+
+'''
+DOM ZAVDANYA 1
+'''
+
+# Ctrl + / -> comment
+
