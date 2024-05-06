ca72ea6741d07b46c1a71aa41bb01e32a4de69a3
RadionBr <radionbr@gmail.com>
2024-05-06 21:29:27 +0300

    Added print with text

--- main.py
+++ main.py
@@ -1,17 +1,67 @@
-print('Hello World!')
+#int -> целое число 12
+#float - drobovoe
+#bool - true\false
+#str - riad\nabor simvolob
 
-print("Hello\nworld")
 
-print("Hello\n\tworld")
+#Peremennaya - innovannaya obl oper pamyati
+userName1 = "Rad"
+print(userName1)
+user_name = "Petya"
+print(user_name)
 
-print("Hello\\n\\t\"world\"")
+print(2 ** 3)
+print(14 // 9)
+print(2 % 3)
 
-print("\\\\\\\\\\")
 
 '''
-DOM ZAVDANYA 1
+UROK #2
+number - peremennaya kotoraya sohranyaet dannie
+number = 10 + 7 - 5 budet schitat
+print(type(number)) pokajet kakoe je tip dannih
+peremennaya doljna sootvetstvovat kontentu
+Peremennaya s bukvi (tolko na angl pisat)
+
+Math operations
++ - * / 
+** vozvedenie v stepen
+// celochislennoe
+% ostatok ot delenya
 '''
 
-# Ctrl + / -> comment
+# number = int(input("Enter a number: "))
+# n1 = number // 100
+# #n2 = number // 10 % 10
+# n2 = number % 100 // 10
+# n3 = number % 10
+#
+# print(n1)
+# print(n2)
+# print(n3)
+#
+# result = n1 + n2 + n3
+# print("Result:", result)
 
-print("test")
+##########
+
+# name = input("Enter your name: ")
+# age = int(input("Enter your age: "))
+# #v1
+# print("Hello", name, "Your age", age, "Years old")
+# #v2 konkatinaciya
+# print("Hello " + name + " you are " + str(age) + "years old ")
+# #v3 interpolyacia - peremnnie v riad s funkciey format
+# print(f"Hello, {name}. You {age} old") #f - format riadka
+#
+# age_after_ten_years = age + 10
+# print(age_after_ten_years)
+
+# number: int = 10
+# print(number)
+# print(type(number))
+
+###
+number = 10
+print(number :=10) #моржовый оператор
+###
